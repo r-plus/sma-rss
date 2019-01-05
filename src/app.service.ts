41736fa7ac9b3935a2e504aa4dd00481ccccb643
r-plus <anasthasia.r@gmail.com>
2019-01-05 19:38:46 +0900

    update toppage

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return process.env.NODE_ENV;
+    return '<a href="https://github.com/r-plus/sma-rss">SMA-RSS</a>';
   }
 }
